e76cd079834e6715f85ee590f8cb78593ff997d6
jamesowens <jamesowensdev@gmail.com>
2026-05-08 16:35:45 +0100

    improved metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,19 @@
 // app/layout.tsx
 import "./globals.css";
+import { Metadata } from "next";
+
 import Header from "@/components/ui/Header";
 import CustomCursor from "@/components/ui/CustomCursor";
 import HeatmapBackground from "@/components/ui/HeatmapBackground";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://jamesowens.xyz"),
+  title: "James Owens // Interactive Portfolio",
+  description: "PhD Researcher in Spatial Ecology and Data Scientist.",
+};
 
 export default function RootLayout({
   children,
